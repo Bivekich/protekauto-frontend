ad41872eb017e59a03bbb65622fc39a2f1a652fd
dr1m11 <marat3389@gmail.com>
2025-01-13 22:30:39 +0300

    chore: базовая настройка проекта: FSD, shadcn, prettier, husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierConfig from 'eslint-config-prettier';
+import prettier from 'eslint-plugin-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      prettier: prettier,
+    },
+  },
+  prettierConfig,
 ];
 
 export default eslintConfig;
