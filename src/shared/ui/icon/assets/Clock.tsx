5735daf81af33a807e497d411240cb0719cc6c5e
dr1m11 <marat3389@gmail.com>
2025-01-30 23:31:42 +0300

    fix: Исправлен шрифт, отступы и иконки страницы контактов

--- src/shared/ui/icon/assets/Clock.tsx
+++ src/shared/ui/icon/assets/Clock.tsx
@@ -9,37 +9,10 @@ const Clock: FC<SVGProps<SVGSVGElement>> = (props) => (
     fill="none"
     viewBox="0 0 26 26"
   >
-    <defs>
-      <clipPath id="clip6_452">
-        <rect
-          id="ic:baseline-access-time-filled"
-          width="25"
-          height="25"
-          fill="#fff"
-          fillOpacity="0"
-          rx="0"
-          transform="translate(.5 .5)"
-        ></rect>
-      </clipPath>
-    </defs>
-    <rect
-      id="ic:baseline-access-time-filled"
-      width="25"
-      height="25"
-      fill="#FFF"
-      fillOpacity="0"
-      rx="0"
-      transform="translate(.5 .5)"
-    ></rect>
-    <g clipPath="url(#clip6_452)">
-      <path
-        id="Vector"
-        fill="#C00D0D"
-        fillOpacity="1"
-        fillRule="nonzero"
-        d="M12.98 2.16C7 2.16 2.16 7.01 2.16 13c0 5.97 4.84 10.83 10.82 10.83 6 0 10.85-4.86 10.85-10.83 0-5.99-4.85-10.84-10.85-10.84m3.58 15.94-4.65-4.66V7.58h2.17v4.97l4.02 4.02z"
-      ></path>
-    </g>
+    <path
+      fill="#C00D0D"
+      d="M12.989 2.167C7.009 2.167 2.165 7.02 2.165 13s4.843 10.834 10.823 10.834c5.99 0 10.844-4.854 10.844-10.834S18.979 2.167 12.989 2.167m3.575 15.936-4.648-4.659v-5.86h2.167v4.972l4.019 4.02z"
+    ></path>
   </svg>
 );
 
--- src/widgets/contacts/info/contactsInfo.tsx
+++ src/widgets/contacts/info/contactsInfo.tsx
@@ -7,7 +7,7 @@ import { Icon } from '@/shared';
 export const ContactsInfo = () => {
   return (
     <div
-      className={'w-full flex flex-col h-full max-h-[450px] justify-between'}
+      className={'w-full flex flex-col h-full max-h-[450px] justify-end gap-14'}
     >
       <div className={'space-y-10'}>
         <h1 className={'text-title'}>Контакты</h1>
@@ -15,7 +15,7 @@ export const ContactsInfo = () => {
           {contactsList.map((contact) => (
             <div key={contact.text} className={'flex items-center gap-4'}>
               <Icon name={contact.icon} />
-              <p className={'text-paragraph'}>{contact.text}</p>
+              <p className={'text-paragraph font-semibold'}>{contact.text}</p>
             </div>
           ))}
         </div>
