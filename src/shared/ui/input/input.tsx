9768bf04f300a7ce836faadfcfb5741fc8c0e09b
dr1m11 <marat3389@gmail.com>
2025-02-02 01:00:49 +0300

    feat: Переписан хедер под новый дизайн, обновлена цветовая палитра

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/shared/lib/css';
 import { cva, VariantProps } from 'class-variance-authority';
 
 const inputSizes = {
-  default: 'h-9 px-4 py-2',
+  default: 'h-16 px-7 py-5',
   sm: 'h-8 rounded-md px-3 text-xs',
   lg: 'h-10 rounded-md px-8',
   xl: 'h-12 rounded-md px-3 text-normal',
@@ -17,7 +17,7 @@ const inputVariants = cva(
     variants: {
       variant: {
         default:
-          'variant-caption bg-sky-blue placeholder:text-placeholder rounded-[10px]',
+          'variant-caption bg-white placeholder:text-placeholder rounded-md',
       },
       size: inputSizes,
     },
