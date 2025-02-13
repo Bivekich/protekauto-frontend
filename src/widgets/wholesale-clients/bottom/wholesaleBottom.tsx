43ca4f6c0c585362880b13ae4d81c3ae0581af89
dr1m11 <marat3389@gmail.com>
2025-02-13 22:16:59 +0300

    feat: Добавлена страница Оптовикам

--- src/widgets/wholesale-clients/bottom/wholesaleBottom.tsx
+++ src/widgets/wholesale-clients/bottom/wholesaleBottom.tsx
@@ -1,4 +1,4 @@
-import { Button, Icon, WidgetContainer } from '@/shared';
+import { Icon, WidgetContainer } from '@/shared';
 import Link from 'next/link';
 import styles from './styles.module.css';
 import Image from 'next/image';
